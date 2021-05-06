dd545e05fc80e7596e2d767858348c271beebb48
Rahul Negi <rahulnegi2021@gmail.com>
2021-05-06 12:17:33 +0530

    Removed Venv

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rahulnegi/projects/Webapps/Book_Advisor/Book-Advisor/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
